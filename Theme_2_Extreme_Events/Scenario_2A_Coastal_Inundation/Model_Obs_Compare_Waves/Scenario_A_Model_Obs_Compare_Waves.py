e978295af52fb409d752b1574807be9349be0c20
Bob Fratantonio <bobfrat@gmail.com>
2014-06-05 11:19:12 -0400

    Theme 2 Scenario A Waves
    - Adhere to new directory structure
    - Add model data

--- Theme_2_Extreme_Events/Scenario_2A_Coastal_Inundation/Model_Obs_Compare_Waves/Scenario_A_Model_Obs_Compare_Waves.py
+++ Theme_2_Extreme_Events/Scenario_2A_Coastal_Inundation/Model_Obs_Compare_Waves/Scenario_A_Model_Obs_Compare_Waves.py
@@ -3,12 +3,22 @@
 
 # <markdowncell>
 
-# ># IOOS System Test: [Extreme Events Theme:](https://github.com/ioos/system-test/wiki/Development-of-Test-Themes#theme-2-extreme-events) Inundation
+# ># IOOS System Test: [Extreme Events Theme:](https://github.com/ioos/system-test/wiki/Development-of-Test-Themes#theme-2-extreme-events) Coastal Inundation
 
 # <markdowncell>
 
 # ### Can we compare observed and modeled wave parameters? 
-# This notebook is based on [IOOS System Test: Inundation](http://nbviewer.ipython.org/github/rsignell-usgs/notebook/blob/fef9438303b49a923024892db1ef3115e34d8271/CSW/IOOS_inundation.ipynb) by Rich Signell
+# This notebook is based on [IOOS System Test: Inundation](http://nbviewer.ipython.org/github/ioos/system-test/blob/master/Theme_2_Extreme_Events/Scenario_2A_Coastal_Inundation/Scenario_2A_Water_Level_Signell.ipynb)
+# 
+# Methodology:
+# * Define temporal and spatial bounds of interest, as well as parameters of interest
+# * Search for available service endpoints in the NGDC CSW catalog meeting search criteria
+# * Extract OPeNDAP data endpoints from model datasets and SOS endpoints from observational datasets
+# * Obtain observation data sets from stations within the spatial boundaries
+# * Plot observation stations on a map (red marker if not enough data)
+# * Using DAP (model) endpoints find all available models data sets that fall in the area of interest, for the specified time range, and extract a model grid cell closest to all the given station locations
+# * Plot modelled and observed time series wave data on same axes for comparison
+# 
 
 # <headingcell level=4>
 
@@ -16,37 +26,21 @@
 
 # <codecell>
 
-import sys
-import csv
-import random
-import parser
-import cStringIO
+import datetime as dt
+from warnings import warn
 
+import folium
+from IPython.display import HTML
+import iris
+from iris.exceptions import CoordinateNotFoundError, ConstraintMismatchError
 import matplotlib.pyplot as plt
-from pylab import *
-from pyoos.collectors.ndbc.ndbc_sos import NdbcSos
 from owslib.csw import CatalogueServiceWeb
 from owslib import fes
-import netCDF4
 import pandas as pd
-import datetime as dt
-import dateutil.parser as duparser
-import iris
-import urllib2
-from lxml import etree       #TODO suggest using bs4 instead for ease of access to XML objects
-import requests              #required for the processing of requests
-from bs4 import *            #required for the xml parsing of getcaps and get obs
-from IPython.display import HTML
-import folium #required for leaflet mapping
-import sqlite3 as lite
+from pyoos.collectors.ndbc.ndbc_sos import NdbcSos
+import requests
 
-from utilities import dateRange
-from utilities import get_coordinates # required for mapping the coordinates
-from utilities import service_urls
-
-# <markdowncell>
-
-# some functions from [Rich Signell Notebook](http://nbviewer.ipython.org/github/rsignell-usgs/notebook/blob/fef9438303b49a923024892db1ef3115e34d8271/CSW/IOOS_inundation.ipynb)
+from utilities import date_range, coops2df, find_timevar, find_ij, nearxy, service_urls, mod_df, get_coordinates
 
 # <headingcell level=4>
 
@@ -54,14 +48,18 @@ from utilities import service_urls
 
 # <codecell>
 
-#bounding box of interest,[bottom right[lat,lon], top left[lat,lon]]
 bounding_box_type = "box" 
-bounding_box = [[-77,34],[-70,44]]
+# Specify large bounding box around mid Atlantic coast
+# [bottom right[lat,lon], top left[lat,lon]]
+bounding_box = [[-77,34],[-70,40]]  
 
-#temporal range
+#temporal range - May 1 2014 - May 10 2014
 start_date = dt.datetime(2014,5,1,0,50).strftime('%Y-%m-%d %H:%M')
 end_date = dt.datetime(2014,5,10).strftime('%Y-%m-%d %H:00')
 time_date_range = [start_date,end_date]  #start_date_end_date
+
+jd_start = dt.datetime.strptime(start_date, '%Y-%m-%d %H:%M')
+jd_stop = dt.datetime.strptime(end_date, '%Y-%m-%d %H:%M')
 
 print start_date,'to',end_date
 
@@ -84,7 +82,6 @@ data_dict["waves"] = {"names":['sea_surface_wave_significant_height','significan
 # <codecell>
 
 endpoint = 'http://www.ngdc.noaa.gov/geoportal/csw' # NGDC Geoportal
-#endpoint = 'http://data.nodc.noaa.gov/geoportal/csw'  # NODC Geoportal: collection level 
 csw = CatalogueServiceWeb(endpoint,timeout=60)
 
 for oper in csw.operations:
@@ -94,7 +91,7 @@ for oper in csw.operations:
 # <codecell>
 
 # convert User Input into FES filters
-start,stop = dateRange(start_date,end_date)
+start,stop = date_range(start_date,end_date)
 box = []
 box.append(bounding_box[0][0])
 box.append(bounding_box[0][1])
@@ -155,21 +152,6 @@ iso_end = end_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 # <codecell>
 
-def get_station_long_name(sta):
-    """
-    get longName for specific station
-    """
-    #http://sdf.ndbc.noaa.gov/sos/server.php?request=DescribeSensor&service=SOS&version=1.0.0&outputformat=text/xml;subtype=%22sensorML/1.0.1%22&procedure=urn:ioos:station:wmo:41012
-    url=(sos_urls[0]+'?service=SOS&version=1.0.0&'
-        'request=DescribeSensor&version=1.0.0&outputFormat=text/xml;subtype="sensorML/1.0.1"&'
-        'procedure=urn:ioos:station:wmo:%s') % sta    
-    tree = etree.parse(urllib2.urlopen(url))
-    root = tree.getroot()
-    longName=root.xpath("//sml:identifier[@name='longName']/sml:Term/sml:value/text()", namespaces={'sml':"http://www.opengis.net/sensorML/1.0.1"})
-    return longName
-
-# <codecell>
-
 collector = NdbcSos()
 print collector.server.identification.title
 collector.start_time = start_time
@@ -209,32 +191,12 @@ obs_loc_df.head()
 # <codecell>
 
 stations = [sta.split(':')[-1] for sta in obs_loc_df['station_id']]
-#obs_lon = [sta for sta in obs_loc_df['longitude (degree)']]
-#obs_lat = [sta for sta in obs_loc_df['latitude (degree)']]
+obs_lon = [sta for sta in obs_loc_df['longitude (degree)']]
+obs_lat = [sta for sta in obs_loc_df['latitude (degree)']]
 
 # <headingcell level=3>
 
 # Request CSV response from SOS and convert to Pandas DataFrames
-
-# <codecell>
-
-def coops2df(collector, coops_id, sos_name):
-    collector.features = [coops_id]
-    collector.variables = [sos_name]
-    response = collector.raw(responseFormat="text/csv")
-    data_df = pd.read_csv(cStringIO.StringIO(str(response)), parse_dates=True, index_col='date_time')
-#    data_df['Observed Data']=data_df['water_surface_height_above_reference_datum (m)']-data_df['vertical_position (m)']
-    data_df['Observed Wave Height Data'] = data_df['sea_surface_wave_significant_height (m)']
-    data_df['Observed Peak Period Data'] = data_df['sea_surface_wave_peak_period (s)']
-
-    a = get_station_long_name(coops_id)
-    if len(a) == 0:
-        long_name = coops_id
-    else:
-        long_name = a[0]
-        
-    data_df.name = long_name
-    return data_df
 
 # <codecell>
 
@@ -269,9 +231,7 @@ lon_center = abs(bounding_box[0][0]-bounding_box[1][0])/2 + bounding_box[0][0]
 map = folium.Map(location=[lat_center, lon_center], zoom_start=5)
 
 for n in range(len(obs_loc_df)):
-#for station in stations:
     #get the station data from the sos end point
-    
     shortname = obs_loc_df['station_id'][n].split(':')[-1]
     longname = Hs_obs_df[n].name
     lat = obs_loc_df['latitude (degree)'][n]
@@ -296,8 +256,10 @@ for n in range(len(Hs_obs_df)):
         fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20,5))
         Hs_obs_df[n].plot(ax=axes[0], color='r')
         axes[0].set_title(Hs_obs_df[n].name)
+        axes[0].set_ylabel('Hs (m)')
         Tp_obs_df[n].plot(ax=axes[1])
         axes[1].set_title(Tp_obs_df[n].name)
+        axes[1].set_ylabel('Tp (s)')
 
 
 
@@ -305,4 +267,92 @@ for n in range(len(Hs_obs_df)):
 
 # ###Get model output from OPeNDAP URLS
 # Try to open all the OPeNDAP URLS using Iris from the British Met Office. If we can open in Iris, we know it's a model result.
+
+# <codecell>
+
+print data_dict['waves']['names']
+name_in_list = lambda cube: cube.standard_name in data_dict['waves']['names']
+constraint = iris.Constraint(cube_func=name_in_list)
+ 
+
+# <codecell>
+
+# Use only data within 0.04 degrees (about 4 km).
+max_dist = 0.04
+# Use only data where the standard deviation of the time series exceeds 0.01 m (1 cm).
+# This eliminates flat line model time series that come from land points that should have had missing values.
+min_var = 0.01
+for url in dap_urls:
+    try:
+        a = iris.load_cube(url, constraint)
+        # convert to units of meters
+        # a.convert_units('m')     # this isn't working for unstructured data
+        # take first 20 chars for model name
+        mod_name = a.attributes['title'][0:20]
+        r = a.shape
+        timevar = find_timevar(a)
+        lat = a.coord(axis='Y').points
+        lon = a.coord(axis='X').points
+        jd = timevar.units.num2date(timevar.points)
+        start = timevar.units.date2num(jd_start)
+        istart = timevar.nearest_neighbour_index(start)
+        stop = timevar.units.date2num(jd_stop)
+        istop = timevar.nearest_neighbour_index(stop)
+
+        # Only proceed if we have data in the range requested.
+        if istart != istop:
+            nsta = len(obs_lon)
+            if len(r) == 3:
+                print('[Structured grid model]:', url)
+                d = a[0, :, :].data
+                # Find the closest non-land point from a structured grid model.
+                if len(lon.shape) == 1:
+                    lon, lat = np.meshgrid(lon, lat)
+                j, i, dd = find_ij(lon, lat, d, obs_lon, obs_lat)
+                for n in range(nsta):
+                    # Only use if model cell is within 0.01 degree of requested
+                    # location.
+                    if dd[n] <= max_dist:
+                        arr = a[istart:istop, j[n], i[n]].data
+                        if arr.std() >= min_var:
+                            c = mod_df(arr, timevar, istart, istop,
+                                       mod_name, ts)
+                            name = Hs_obs_df[n].name
+                            Hs_obs_df[n] = pd.concat([Hs_obs_df[n], c], axis=1)
+                            Hs_obs_df[n].name = name
+            elif len(r) == 2:
+                print('[Unstructured grid model]:', url)
+                # Find the closest point from an unstructured grid model.
+                index, dd = nearxy(lon.flatten(), lat.flatten(),
+                                   obs_lon, obs_lat)
+                for n in range(nsta):
+                    # Only use if model cell is within 0.1 degree of requested
+                    # location.
+                    if dd[n] <= max_dist:
+                        arr = a[istart:istop, index[n]].data
+                        if arr.std() >= min_var:
+                            c = mod_df(arr, timevar, istart, istop,
+                                       mod_name, ts)
+                            name = Hs_obs_df[n].name
+                            Hs_obs_df[n] = pd.concat([Hs_obs_df[n], c], axis=1)
+                            Hs_obs_df[n].name = name
+            elif len(r) == 1:
+                print('[Data]:', url)
+    except (ValueError, RuntimeError, CoordinateNotFoundError,
+            ConstraintMismatchError) as e:
+        warn("\n%s\n" % e)
+        pass
+
+# <markdowncell>
+
+# #### Plot Modeled vs Obs Wave Height
+
+# <codecell>
+
+for n in range(len(Hs_obs_df)):
+    if Hs_obs_df[n]['Observed Wave Height Data'].count() > min_data_pts:
+        ax = Hs_obs_df[n].plot(figsize=(14, 6), title=Hs_obs_df[n].name, legend=False)
+        plt.setp(ax.lines[0], linewidth=4.0, color='0.7', zorder=1, marker='.')
+        ax.legend()
+        ax.set_ylabel('m')
 
