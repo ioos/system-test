a1ec228d7f95f9d8c63ace1ad2a81633dfe56c97
Bob Fratantonio <bobfrat@gmail.com>
2014-08-21 15:55:13 -0400

    Updates to the model obs waves comparison notebook
    - Use pyoos bounding box filter
    - added error checking
    - improve readability

--- Theme_2_Extreme_Events/Scenario_2A_Coastal_Inundation/Scenario_2A_ModelDataCompare_Waves/Scenario_A_Model_Obs_Compare_Waves.py
+++ Theme_2_Extreme_Events/Scenario_2A_Coastal_Inundation/Scenario_2A_ModelDataCompare_Waves/Scenario_A_Model_Obs_Compare_Waves.py
@@ -28,8 +28,9 @@
 
 import datetime as dt
 from warnings import warn
-
+from io import BytesIO
 import folium
+import netCDF4
 from IPython.display import HTML
 import iris
 from iris.exceptions import CoordinateNotFoundError, ConstraintMismatchError
@@ -40,7 +41,8 @@ import pandas as pd
 from pyoos.collectors.ndbc.ndbc_sos import NdbcSos
 import requests
 
-from utilities import fes_date_filter, coops2df, find_timevar, find_ij, nearxy, service_urls, mod_df, get_coordinates
+from utilities import (fes_date_filter, collector2df, find_timevar, find_ij, nearxy, service_urls, mod_df, 
+                       get_coordinates, get_station_longName, inline_map)
 
 # <headingcell level=4>
 
@@ -49,9 +51,20 @@ from utilities import fes_date_filter, coops2df, find_timevar, find_ij, nearxy, 
 # <codecell>
 
 bounding_box_type = "box" 
-# Specify large bounding box around mid Atlantic coast
-# [bottom right[lat,lon], top left[lat,lon]]
-bounding_box = [[-77,34],[-70,40]]  
+
+# Bounding Box [lon_min, lat_min, lon_max, lat_max]
+area = {'Hawaii': [-160.0, 18.0, -154., 23.0],
+        'Gulf of Maine': [-72.0, 41.0, -69.0, 43.0],
+        'New York harbor region': [-75., 39., -71., 41.5],
+        'Puerto Rico': [-75, 12, -55, 26],
+        'East Coast': [-77, 34, -70, 40],
+        'North West': [-130, 38, -121, 50],
+        'Gulf of Mexico': [-92, 28, -84, 31],
+        'Arctic': [-179, 63, -140, 80],
+        'North East': [-74, 40, -69, 42],
+        'Virginia Beach': [-76, 34, -74, 38]}
+
+bounding_box = area['East Coast']
 
 #temporal range - May 1 2014 - May 10 2014
 start_date = dt.datetime(2014,5,1,0,50).strftime('%Y-%m-%d %H:%M')
@@ -70,9 +83,15 @@ print start_date,'to',end_date
 # <codecell>
 
 #put the names in a dict for ease of access 
+# put the names in a dict for ease of access 
 data_dict = {}
 sos_name = 'waves'
-data_dict["waves"] = {"names":['sea_surface_wave_significant_height','significant_wave_height','sea_surface_wave_significant_height(m)', 'sea_surface_wave_significant_height (m)'], 
+data_dict["waves"] = {"names":['sea_surface_wave_significant_height',
+                               'significant_wave_height',
+                               'significant_height_of_wave',
+                               'sea_surface_wave_significant_height(m)',
+                               'sea_surface_wave_significant_height (m)',
+                               'water_surface_height'], 
                       "sos_name":["waves"]}  
 
 # <headingcell level=3>
@@ -84,36 +103,25 @@ data_dict["waves"] = {"names":['sea_surface_wave_significant_height','significan
 endpoint = 'http://www.ngdc.noaa.gov/geoportal/csw' # NGDC Geoportal
 csw = CatalogueServiceWeb(endpoint,timeout=60)
 
-for oper in csw.operations:
-    if oper.name == 'GetRecords':
-        print '\nISO Queryables:\n',oper.constraints['SupportedISOQueryables']['values']
-
 # <codecell>
 
 # convert User Input into FES filters
 start,stop = fes_date_filter(start_date,end_date)
-box = []
-box.append(bounding_box[0][0])
-box.append(bounding_box[0][1])
-box.append(bounding_box[1][0])
-box.append(bounding_box[1][1])
-bbox = fes.BBox(box)
+bbox = fes.BBox(bounding_box)
 
 #use the search name to create search filter
-or_filt = fes.Or([fes.PropertyIsLike(propertyname='apiso:AnyText',literal=('*%s*' % val),
+or_filt = fes.Or([fes.PropertyIsLike(propertyname='apiso:AnyText',literal='*%s*' % val,
                     escapeChar='\\',wildCard='*',singleChar='?') for val in data_dict["waves"]["names"]])
-val = 'Averages'
-not_filt = fes.Not([fes.PropertyIsLike(propertyname='apiso:AnyText',literal=('*%s*' % val),
-                        escapeChar='\\',wildCard='*',singleChar='?')])
-filter_list = [fes.And([ bbox, start, stop, or_filt, not_filt]) ]
-# connect to CSW, explore it's properties
+
 # try request using multiple filters "and" syntax: [[filter1,filter2]]
+filter_list = [fes.And([ bbox, start, stop, or_filt]) ]
+
 csw.getrecords2(constraints=filter_list,maxrecords=1000,esn='full')
 print str(len(csw.records)) + " csw records found"
 
 # <markdowncell>
 
-# Dap URLS
+# #### Dap URLs
 
 # <codecell>
 
@@ -122,26 +130,24 @@ dap_urls = service_urls(csw.records)
 dap_urls = sorted(set(dap_urls))
 print "Total DAP:",len(dap_urls)
 #print the first 5...
-print "\n".join(dap_urls[1:5])
+print "\n".join(dap_urls[0:5])
 
 # <markdowncell>
 
-# SOS URLs
+# #### SOS URLs
 
 # <codecell>
 
 sos_urls = service_urls(csw.records,service='sos:url')
 #remove duplicates and organize
-#if len(sos_urls) ==0:
-sos_urls.append("http://sdf.ndbc.noaa.gov/sos/server.php")  #?request=GetCapabilities&service=SOS
-
 sos_urls = sorted(set(sos_urls))
+
 print "Total SOS:",len(sos_urls)
 print "\n".join(sos_urls)
 
 # <markdowncell>
 
-# ### SOS Requirements
+# ###Get most recent observations from all stations in bounding box
 
 # <codecell>
 
@@ -150,39 +156,26 @@ end_time = dt.datetime.strptime(end_date,'%Y-%m-%d %H:%M')
 iso_start = start_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 iso_end = end_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-# <codecell>
-
+# Define the SOS collector
 collector = NdbcSos()
 print collector.server.identification.title
-collector.start_time = start_time
-collector.end_time = end_time
 collector.variables = data_dict["waves"]["sos_name"]
 collector.server.identification.title
-print collector.start_time,":", collector.end_time
+
+# Don't specify start and end date in the filter and the most recent observation will be returned
+collector.filter(bbox=bounding_box,
+                 variables=data_dict["waves"]["sos_name"])
+
+response = collector.raw(responseFormat="text/csv")
+obs_loc_df = pd.read_csv(BytesIO(response.encode('utf-8')),
+                         parse_dates=True,
+                         index_col='date_time')
+
+# Now let's specify start and end times
+collector.start_time = start_time
+collector.end_time = end_time
+
 ofrs = collector.server.offerings
-
-# <markdowncell>
-
-# ###Find all SOS stations within the bounding box and time extent
-# The time extent (iso_start) must be a match a timestamp in the data set. This buoy collects data hourly at the 50 minute mark.
-
-# <codecell>
-
-print "Date: ",iso_start," to ", iso_end
-box_str=','.join(str(e) for e in box)
-print "Lat/Lon Box: ",box_str
-
-url=(sos_urls[0]+'?'
-     'service=SOS&request=GetObservation&version=1.0.0&'
-     'offering=urn:ioos:network:noaa.nws.ndbc:all&observedProperty=%s&'
-     'responseFormat=text/csv&featureOfInterest=BBOX:%s') % ("waves",box_str)
-#url=(sos_urls[0]+'?'
-#     'service=SOS&request=GetObservation&version=1.0.0&'
-#     'offering=urn:ioos:station:wmo:%s&observedProperty=%s&'
-#     'responseFormat=text/csv&eventTime=%s|%s&featureOfInterest=BBOX:%s') % (wmo_id,"waves",iso_start,iso_stop,box_str)
-
-print url
-obs_loc_df = pd.read_csv(url)
 
 # <codecell>
 
@@ -203,65 +196,69 @@ obs_lat = [sta for sta in obs_loc_df['latitude (degree)']]
 ts_rng = pd.date_range(start=start_date, end=end_date)
 ts = pd.DataFrame(index=ts_rng)
 
+# Save all of the observation data into a list of dataframes
+obs_df = []
+# Create a list of dataframes for just wave heights for comparing with modeled wave heights later
 Hs_obs_df = []
-Tp_obs_df = []
 
 for sta in stations:
-    b=coops2df(collector, sta, sos_name)
-    # limit interpolation to 10 points (10 @ 6min = 1 hour)
-    Hs_obs_df.append(pd.DataFrame(pd.concat([b, ts],axis=1)['Observed Wave Height Data']))
-    Hs_obs_df[-1].name=b.name
-    Tp_obs_df.append(pd.DataFrame(pd.concat([b, ts],axis=1)['Observed Peak Period Data']))
-    Tp_obs_df[-1].name=b.name
+    raw_df = collector2df(collector, sta, sos_name)
+    obs_df.append(pd.DataFrame(pd.concat([raw_df, ts],axis=1)))
+    obs_df[-1].name = raw_df.name
+    if raw_df.empty:
+        Hs_obs_df.append(pd.DataFrame())
+    else:
+        Hs_obs_df.append(pd.DataFrame(pd.concat([raw_df, ts],axis=1)['sea_surface_wave_significant_height (m)']))
+
+    Hs_obs_df[-1].name = raw_df.name
     
+
+# <markdowncell>
+
+# ### Plot the Observation Stations on Map
 
 # <codecell>
 
-# Define minimum amount of data points to plot
 min_data_pts = 20
 
-#Embeds the HTML source of the map directly into the IPython notebook.
-def inline_map(map):   
-    map._build_map()
-    return HTML('<iframe srcdoc="{srcdoc}" style="width: 100%; height: 500px; border: none"></iframe>'.format(srcdoc=map.HTML.replace('"', '&quot;')))
+# Find center of bounding box
+lat_center = abs(bounding_box[3]-bounding_box[1])/2 + bounding_box[1]
+lon_center = abs(bounding_box[0]-bounding_box[2])/2 + bounding_box[0]
+m = folium.Map(location=[lat_center, lon_center], zoom_start=6)
 
-#find center of bounding box
-lat_center = abs(bounding_box[1][1] - bounding_box[0][1])/2 + bounding_box[0][1]
-lon_center = abs(bounding_box[0][0]-bounding_box[1][0])/2 + bounding_box[0][0]
-map = folium.Map(location=[lat_center, lon_center], zoom_start=5)
-
-for n in range(len(obs_loc_df)):
+n = 0
+for df in obs_df:
     #get the station data from the sos end point
-    shortname = obs_loc_df['station_id'][n].split(':')[-1]
-    longname = Hs_obs_df[n].name
+    longname = df.name
     lat = obs_loc_df['latitude (degree)'][n]
     lon = obs_loc_df['longitude (degree)'][n]
     popup_string = ('<b>Station:</b><br>'+ longname)
-    if len(Hs_obs_df[n]) > min_data_pts:
-        map.simple_marker([lat, lon], popup=popup_string)
+    if len(df) > min_data_pts:
+        m.simple_marker([lat, lon], popup=popup_string)
     else:
         #popup_string += '<br>No Data Available'
         popup_string += '<br>Not enough data available<br>requested pts: ' + str(min_data_pts ) + '<br>Available pts: ' + str(len(Hs_obs_df[n]))
-        map.circle_marker([lat, lon], popup=popup_string, fill_color='#ff0000', radius=10000, line_color='#ff0000')
+        m.circle_marker([lat, lon], popup=popup_string, fill_color='#ff0000', radius=10000, line_color='#ff0000')
+    n += 1
+m.line(get_coordinates(bounding_box,bounding_box_type), line_color='#FF0000', line_weight=5)
 
-map.line(get_coordinates(bounding_box,bounding_box_type), line_color='#FF0000', line_weight=5)
-
-inline_map(map)
+inline_map(m)
 
 # <codecell>
 
-# Plot Hs and Tp for each station
-for n in range(len(Hs_obs_df)):
-    if len(Hs_obs_df[n]) > min_data_pts:
+### Plot Hs and Tp for each station
+
+# <codecell>
+
+for df in obs_df:
+    if len(df) > min_data_pts:
         fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20,5))
-        Hs_obs_df[n].plot(ax=axes[0], color='r')
-        axes[0].set_title(Hs_obs_df[n].name)
+        df['sea_surface_wave_significant_height (m)'].plot(ax=axes[0], color='r')
+        axes[0].set_title(df.name)
         axes[0].set_ylabel('Hs (m)')
-        Tp_obs_df[n].plot(ax=axes[1])
-        axes[1].set_title(Tp_obs_df[n].name)
+        df['sea_surface_wave_peak_period (s)'].plot(ax=axes[1])
+        axes[1].set_title(df.name)
         axes[1].set_ylabel('Tp (s)')
-
-
 
 # <markdowncell>
 
@@ -270,10 +267,8 @@ for n in range(len(Hs_obs_df)):
 
 # <codecell>
 
-print data_dict['waves']['names']
 name_in_list = lambda cube: cube.standard_name in data_dict['waves']['names']
 constraint = iris.Constraint(cube_func=name_in_list)
- 
 
 # <codecell>
 
@@ -283,10 +278,11 @@ max_dist = 0.04
 # This eliminates flat line model time series that come from land points that should have had missing values.
 min_var = 0.01
 for url in dap_urls:
+    if 'cdip' in url:
+        # The CDIP buoys are known to be observed data, so let's just skip
+        continue
     try:
         a = iris.load_cube(url, constraint)
-        # convert to units of meters
-        # a.convert_units('m')     # this isn't working for unstructured data
         # take first 20 chars for model name
         mod_name = a.attributes['title'][0:20]
         r = a.shape
@@ -301,7 +297,7 @@ for url in dap_urls:
 
         # Only proceed if we have data in the range requested.
         if istart != istop:
-            nsta = len(obs_lon)
+            nsta = len(stations)
             if len(r) == 3:
                 print('[Structured grid model]:', url)
                 d = a[0, :, :].data
@@ -345,13 +341,14 @@ for url in dap_urls:
 
 # <markdowncell>
 
-# #### Plot Modeled vs Obs Wave Height
+# ### Plot Modeled vs Obs Wave Height
 
 # <codecell>
 
-for n in range(len(Hs_obs_df)):
-    if Hs_obs_df[n]['Observed Wave Height Data'].count() > min_data_pts:
-        ax = Hs_obs_df[n].plot(figsize=(14, 6), title=Hs_obs_df[n].name, legend=False)
+for df in Hs_obs_df:
+    # Make sure there is obs data at the station for comparison
+    if 'sea_surface_wave_significant_height (m)' in df.columns:
+        ax = df.plot(figsize=(14, 6), title=df.name, legend=False)
         plt.setp(ax.lines[0], linewidth=4.0, color='0.7', zorder=1, marker='.')
         ax.legend()
         ax.set_ylabel('m')
