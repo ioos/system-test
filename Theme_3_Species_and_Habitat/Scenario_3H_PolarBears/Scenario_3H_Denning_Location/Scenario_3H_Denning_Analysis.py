6714f82ad412554beffc587e846fdbc02e42ed9f
ocefpaf <ocefpaf@gmail.com>
2014-11-26 15:05:37 -0300

    Added missing `.py` files.

---
+++ Theme_3_Species_and_Habitat/Scenario_3H_PolarBears/Scenario_3H_Denning_Location/Scenario_3H_Denning_Analysis.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# # IOOS System Test - Theme 3 - Scenario H - [Description](https://github.com/ioos/system-test/wiki/Development-of-Test-Themes#scenario3H)
+# 
+# ## Exploring Environmental Changes Affecting Polar Bears, part 1: Denning Analysis
+# 
+# ## Questions
+# 1. Can we discover, access and map polar bear denning locations?
+# 2. Can we discover, access and map the sea ice edge using NSIDC passive microwave satellite data?
+# 3. What is the distance between polar bear denning locations and the closest sea ice edge through time?
+
+# ## Q1 Can we discover, access and map polar bear denning locations? 
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
